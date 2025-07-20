a2e76805e28ff86a1a4d2d0c0acdd50abc2a88a7
Jeff <jeff.ches155@gmail.com>
2025-07-20 11:31:40 -0400

    implemented calculation_factory class

---
+++ app/models/calculation_factory.py
@@ -0,0 +1,19 @@
+from app.models.calculation import Addition, Subtraction, Multiplication, Division, Calculation
+from app.models.calculation import CalculationType
+
+# The factory replaces manual if/else logic where you choose 
+    # and instantiate calculation subclasses (Addition, Subtraction, etc.) based on the type.
+class CalculationFactory:
+
+    @staticmethod
+    def create_calculation(calc_type: CalculationType, a: float, b: float) -> Calculation:
+        if calc_type == CalculationType.ADDITION:
+            return Addition(a=a, b=b)
+        elif calc_type == CalculationType.SUBTRACTION:
+            return Subtraction(a=a, b=b)
+        elif calc_type == CalculationType.MULTIPLICATION:
+            return Multiplication(a=a, b=b)
+        elif calc_type == CalculationType.DIVISION:
+            return Division(a=a, b=b)
+        else:
+            raise ValueError(f"Unknown calculation type: {calc_type}")
